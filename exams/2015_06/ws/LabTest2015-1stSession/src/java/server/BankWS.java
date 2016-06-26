a1cad82da8592f81bca7d70394b33178128edef2
giacomolanciano <g.lanciano1@gmail.com>
2016-06-26 14:21:31 +0200

    add code for july 2015 ws exercise

--- exams/2015_06/ws/LabTest2015-1stSession/src/java/server/BankWS.java
+++ exams/2015_06/ws/LabTest2015-1stSession/src/java/server/BankWS.java
@@ -22,7 +22,7 @@ public class BankWS {
      * Naive date implementation to make the computation easier
      */
     private static final int TODAY = 26;
-    private static final int INTERVAL = 3;
+    private static final int INTERVAL = 7;
     
     private static final int OP_ID_POS = 0;
     private static final int CLIENT_ID_POS = 1;
@@ -42,12 +42,12 @@ public class BankWS {
         
         lastOperations.add(new String[]{"0", "0", "17", "200", "blah"});
         lastOperations.add(new String[]{"1", "0", "17", "200", "blah"});
-        lastOperations.add(new String[]{"2", "0", "22", "200", "blah"});
+        lastOperations.add(new String[]{"2", "0", "22", "200", "Benzina Autostrada"});
         lastOperations.add(new String[]{"3", "0", "24", "200", "blah"});
         lastOperations.add(new String[]{"4", "1", "12", "200", "blah"});
         lastOperations.add(new String[]{"5", "1", "24", "200", "blah"});
         lastOperations.add(new String[]{"6", "1", "12", "200", "blah"});
-        lastOperations.add(new String[]{"7", "2", "12", "200", "blah"});
+        lastOperations.add(new String[]{"7", "2", "12", "200", "Benzina Autostrada"});
         lastOperations.add(new String[]{"8", "2", "15", "200", "blah"});
         
     
@@ -78,12 +78,12 @@ public class BankWS {
         
         lastOperations.add(new String[]{"0", "0", "17", "200", "blah"});
         lastOperations.add(new String[]{"1", "0", "17", "200", "blah"});
-        lastOperations.add(new String[]{"2", "0", "22", "200", "blah"});
+        lastOperations.add(new String[]{"2", "0", "22", "200", "Benzina Autostrada"});
         lastOperations.add(new String[]{"3", "0", "24", "200", "blah"});
         lastOperations.add(new String[]{"4", "1", "12", "200", "blah"});
         lastOperations.add(new String[]{"5", "1", "24", "200", "blah"});
         lastOperations.add(new String[]{"6", "1", "12", "200", "blah"});
-        lastOperations.add(new String[]{"7", "2", "12", "200", "blah"});
+        lastOperations.add(new String[]{"7", "2", "12", "200", "Benzina Autostrada"});
         lastOperations.add(new String[]{"8", "2", "15", "200", "blah"});
         
         for(String[] a : lastOperations) {
