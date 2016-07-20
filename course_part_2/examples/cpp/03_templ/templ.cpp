db5e169f76c4a336374743374770e63b5b62738d
giacomolanciano <g.lanciano1@gmail.com>
2016-07-20 17:16:51 +0200

    add comments to c++ template functions examples

--- course_part_2/examples/cpp/03_templ/templ.cpp
+++ course_part_2/examples/cpp/03_templ/templ.cpp
@@ -1,18 +1,25 @@
-template <class T>
-T sum(T x, T y) {
-	return x+y;
-}
-
 int sumint(int x, int y) {
 	return x+y;
 }
 
 /*
-int f() {
-	return sum<int>(10, 20);	//specialize template function
+ * with this implementation we can use the "same" function with many
+ * different data types (~ java generics)
+ * */
+template <class T>
+T sum(T x, T y) {
+	return x+y;
 }
-*/
 
-int f() {
-	return sum<int>(10, 20) + sum<double>(2.5, 7.3);	//looking at assembly the impl of the two is completely different
+int f1() {
+	return sum<int>(10, 20);	//specialize template function for int
+}
+
+
+int f2() {
+	/*
+	 * NOTE: looking at the assembly implementation of the two specializations
+	 * we can see that they are completely different
+	 * */
+	return sum<int>(10, 20) + sum<double>(2.5, 7.3);
 }
--- course_part_2/examples/cpp/03_templ/templ1.cpp
+++ course_part_2/examples/cpp/03_templ/templ1.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 
-template <class T, int N>   //in java cannot have int in generic types, in c++ we can do it as a preprocessing to embedd an instance of a value
+/*
+ * in java cannot have int in generic types, in c++ we can do it as a 
+ * preprocessing to embedd an instance of a value
+ * */
+template <class T, int N>
 T sum(T x){
 	return x + N;
 }
 
 int main() {
 	int x = 30;
-	std::cout << sum<int,10>(x) << "\n";   //10 is hardcoded in the code, specific version of this function with 10 as parameters -> faster!!!
+	/*
+	 * 10 is hardcoded in the code, i.e. a specific version of this function 
+	 * with 10 as parameters -> faster!!!
+	 * */
+	std::cout << sum<int,10>(x) << "\n";
 	return 0;
 }
