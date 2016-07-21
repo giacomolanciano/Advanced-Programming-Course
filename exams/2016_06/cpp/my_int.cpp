1b7c5f8f68098ce5909b1bc8604e5dca5d0a7ce4
giacomolanciano <g.lanciano1@gmail.com>
2016-07-21 14:25:56 +0200

    add code for june 2016 c++ ex

---
+++ exams/2016_06/cpp/my_int.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+/*
+ * create a customized int such that main executes
+ * 
+ * NOTE: compile with '-D DEBUG' for debug messages
+ * */
+class my_int {
+	int x;
+public:
+	my_int() {
+		#ifdef DEBUG
+			std::cout << "empty constructor" << std::endl;
+		#endif
+	}
+	my_int(int a) : x(a) {
+		#ifdef DEBUG
+			std::cout << "1-par constructor" << std::endl;
+		#endif
+	}
+	int getValue() {return x;}
+	my_int operator + (my_int& y) {
+		my_int z = x + y.getValue();
+		return z;
+	}
+};
+ 
+int main() {
+	/*
+	 * empty constructor is called when variables are declared
+	 * */
+	my_int x,y;
+	
+	/*
+	 * 1-par constructor is called when variables are assigned (no need 
+	 * to overload operator '=')
+	 * operator '+' overloading is needed
+	 * */
+	x = 10;
+	y = 20;
+	x = x + y;
+	
+	/*
+	 * getValue() member function is needed
+	 * */
+	std::cout << "values sum up to " << x.getValue() << std::endl;
+	
+	return 0;
+}
